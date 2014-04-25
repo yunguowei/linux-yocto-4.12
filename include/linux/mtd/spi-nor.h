1dcd8fca189bfcfcb1502f914f29227efa1b91eb
Han Xu <han.xu@freescale.com>
2014-04-25 13:40:19 +0800

    MLK-11937: mtd: spi-nor: add DDR quad read support for Micron
    This patch adds the DDR(or DTR) quad read support for the Micron
    SPI NOR flash.
    
    Signed-off-by: Han Xu <b45815@freescale.com>
    [zou:Original patch taken from
    git://git.freescale.com/imx/linux-imx.git imx_4.9.11_1.0.0_ga]
    Signed-off-by: zou cao <cao.zou@windriver.com>

--- include/linux/mtd/spi-nor.h
+++ include/linux/mtd/spi-nor.h
@@ -47,7 +47,8 @@
 #define SPINOR_OP_READ_1_1_2	0x3b	/* Read data bytes (Dual Output SPI) */
 #define SPINOR_OP_READ_1_2_2	0xbb	/* Read data bytes (Dual I/O SPI) */
 #define SPINOR_OP_READ_1_1_4	0x6b	/* Read data bytes (Quad Output SPI) */
-#define SPINOR_OP_READ_1_4_4_D	0xeb	/* Read data bytes (DDR Quad SPI) */
+#define SPINOR_OP_READ_1_1_4_D	0x6d	/* Read data bytes (DDR Quad SPI) */
+#define SPINOR_OP_READ_1_4_4_D	0xed	/* Read data bytes (DDR Quad SPI) */
 #define SPINOR_OP_READ_1_4_4	0xeb	/* Read data bytes (Quad I/O SPI) */
 #define SPINOR_OP_PP		0x02	/* Page program (up to 256 bytes) */
 #define SPINOR_OP_PP_1_1_4	0x32	/* Quad page program */
