392cb33c400df1a0d1d1d9b1fbb246cc7a864e6a
Anson Huang <Anson.Huang@nxp.com>
2016-12-15 21:54:30 +0800

    MLK-13614 input: keyboard: imx: make sure keypad interrupts are enabled in suspend
    Change the suspend/resume callback to suspend_noirq/resume_noirq
    callback to make sure keypad interrupts are enabled during suspend,
    as when there is keypad interrupt coming after suspend callback
    is called, the interrupt handler will still be called and it will
    disable keypad interrupts, it will cause keypad can NOT generate
    interrupts to wake up system from suspend.
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>
    [zou:Original patch taken from
    git://git.freescale.com/imx/linux-imx.git imx_4.9.11_1.0.0_ga]
    Signed-off-by: zou cao <cao.zou@windriver.com>

--- drivers/input/keyboard/imx_keypad.c
+++ drivers/input/keyboard/imx_keypad.c
@@ -528,7 +528,7 @@ static int imx_keypad_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static int __maybe_unused imx_kbd_suspend(struct device *dev)
+static int __maybe_unused imx_kbd_suspend_noirq(struct device *dev)
 {
 	struct platform_device *pdev = to_platform_device(dev);
 	struct imx_keypad *kbd = platform_get_drvdata(pdev);
@@ -555,7 +555,7 @@ static int __maybe_unused imx_kbd_suspend(struct device *dev)
 	return 0;
 }
 
-static int __maybe_unused imx_kbd_resume(struct device *dev)
+static int __maybe_unused imx_kbd_resume_noirq(struct device *dev)
 {
 	struct platform_device *pdev = to_platform_device(dev);
 	struct imx_keypad *kbd = platform_get_drvdata(pdev);
@@ -579,7 +579,10 @@ err_clk:
 	return ret;
 }
 
-static SIMPLE_DEV_PM_OPS(imx_kbd_pm_ops, imx_kbd_suspend, imx_kbd_resume);
+static const struct dev_pm_ops imx_kbd_pm_ops = {
+	.suspend_noirq = imx_kbd_suspend_noirq,
+	.resume_noirq = imx_kbd_resume_noirq,
+};
 
 static struct platform_driver imx_keypad_driver = {
 	.driver		= {
