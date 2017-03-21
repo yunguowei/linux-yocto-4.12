ced304971dcfa73d75ee3f1528d9073ea2422010
Andy Duan <fugang.duan@nxp.com>
2017-03-21 17:59:20 +0800

    MLK-14498-8 tty: serial: imx: add modem device reset
    Add modem device reset, wthether to reset depend on dts configuration.
    
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>
    [zou:Original patch taken from
    git://git.freescale.com/imx/linux-imx.git imx_4.9.11_1.0.0_ga]
    Signed-off-by: zou cao <cao.zou@windriver.com>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -34,6 +34,7 @@
 #include <linux/clk.h>
 #include <linux/delay.h>
 #include <linux/rational.h>
+#include <linux/reset.h>
 #include <linux/slab.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
@@ -1254,8 +1255,16 @@ static void imx_disable_dma(struct imx_port *sport)
 static int imx_startup(struct uart_port *port)
 {
 	struct imx_port *sport = (struct imx_port *)port;
+	struct tty_port *tty_port = &sport->port.state->port;
 	int retval, i;
 	unsigned long flags, temp;
+
+	/* some modem may need reset */
+	if (!(tty_port->flags & ASYNC_SUSPENDED)) {
+		retval = device_reset(sport->port.dev);
+		if (retval && retval != -ENOENT)
+			return retval;
+	}
 
 	retval = clk_prepare_enable(sport->clk_per);
 	if (retval)
@@ -2351,7 +2360,7 @@ static int imx_serial_port_resume_noirq(struct device *dev)
 {
 	struct platform_device *pdev = to_platform_device(dev);
 	struct imx_port *sport = platform_get_drvdata(pdev);
-	int ret;
+	int ret, val;
 
 	ret = clk_enable(sport->clk_ipg);
 	if (ret)
